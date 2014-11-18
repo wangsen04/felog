f02949e8d9de45fbcd2eae80859ab748f6d5cae1
wangsen04 <874066643@qq.com>
2014-11-18 23:28:33 +0800

    save read 初步
    save read 初步

--- routes/read.js
+++ routes/read.js
@@ -10,38 +10,30 @@ var router = express.Router();
 var JsLog = model.JsLog;
 var AjaxLog = model.AjaxLog;
 
+//到查询页面
 router.get('/js/error/query', function (req, res) {
-//    JsLog.remove({creatTime:undefined},function(err){
-//        if (err) return console.error(err);
-//    });
-
     JsLog.find({}, function (err, docs) {
         if (err) return console.error(err);
         console.log( docs);
-
         res.render('read', {title: 'jsLog查询', jsLog: docs,moment:moment});
     });
-
-
 });
 
+//post查询
 router.post('/js/error/query', function (req, res) {
     var domain=req.body.domain;
     var start=req.body.startTime;
     var end=req.body.endTime;
 
-    domain=domain?domain:undefined;
+    reg=/./;
+    domain=domain?domain:reg;
     start=new Date(start);
     end=new Date(end);
 
-    JsLog.find({createTime:{$gt: start, $lt: end}}, function (err, docs) {
+    JsLog.find({domain:domain,createTime:{$gte: start, $lte: end}}, function (err, docs) {
         if (err) return console.error(err);
-        //console.log('------- JsLog.find------' + docs);
-
-        res.render('read', {title: 'jsLog查询', jsLog: docs});
+        res.render('read', {title: 'jsLog查询', jsLog: docs,moment:moment});
     });
-
-
 });
 
 module.exports = router;
